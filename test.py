fb3c88e0b9471fe83ed64aaa27d075a9dda729de
Zoli <zcc6@cornell.edu>
2021-12-08 16:21:06 -0500

    try other hardware pwm library

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import time
 import RPi.GPIO as GPIO
-import pigpio
 
+from rpi_hardware_pwm import HardwarePWM
 import motor_driving
 
 
@@ -11,15 +11,12 @@ def main():
         # time.sleep(10.0)
         # GPIO.cleanup()
 
-        dc = 500000  # 50% duty cycle
-        pwm_pin = 12
-
-        PI = pigpio.pi()
-        PI.hardware_PWM(pwm_pin, 20000, dc)
-
+        pwm = HardwarePWM(0, hz=1000)
+        pwm.start(100) # full duty cycle
         time.sleep(5.0)
-
-        PI.stop()
+        pwm.change_duty_cycle(100)
+        time.sleep(5.0)
+        pwm.stop()
 
     except KeyboardInterrupt:
         GPIO.cleanup()
