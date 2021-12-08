525cd750f0a5ac9d5eba3f4c21897406f0fbfa7a
David Kim (RPi) <jk2537@cornell.edu>
2021-12-08 16:48:18 -0500

    More hardware PWM debugging

--- test.py
+++ test.py
@@ -11,16 +11,19 @@ def main():
         # time.sleep(10.0)
         # GPIO.cleanup()
 
-        pwm = HardwarePWM(0, hz=60)
+        pwm = HardwarePWM(0, hz=6000)
+        pwm2 = HardwarePWM(1, hz=6000)
         md = motor_driving.MotorDriving()
-        md.drive(md.FORWARD)
         pwm.start(100)  # full duty cycle
+        pwm2.start(100)
+        md.drive(md.FORWARD)
         time.sleep(5.0)
 
         md.drive(md.BACKWARD)
         time.sleep(5.0)
 
         pwm.stop()
+        pwm2.stop()
         GPIO.cleanup()
 
     except KeyboardInterrupt:
