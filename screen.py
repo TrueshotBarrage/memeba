0ec6e0a547f06ee892870c836586e49dd53e8982
David Kim <jk2537@cornell.edu>
2021-12-13 14:06:28 -0500

    Updated screen code

--- screen.py
+++ screen.py
@@ -1,14 +1,24 @@
 import os
 import time
+import psutil
+
+
+def kill(procname):
+    for proc in psutil.process_iter():
+        # check whether the process name matches
+        if proc.name() == procname:
+            proc.kill()
 
 
 def init_display():
-    os.system("tightvncserver")
-    os.system("cd ~/memeba")
-    load_meme = "curl -X GET 'https://meme-api.herokuapp.com/gimme' | jq '.url' | xargs wget -O meme; sxiv -a -f -s f meme"
-    os.system(load_meme)
-    time.sleep(5)
-    os.system(load_meme)
+    load_meme = "curl -X GET 'https://meme-api.herokuapp.com/gimme' | jq '.url' | xargs wget -O meme; sxiv -a -f -s f meme &"
+    while True:
+        os.system(load_meme)
+        time.sleep(5)
+        kill("sxiv")
 
 
-init_display()
+try:
+    init_display()
+except KeyboardInterrupt:
+    kill("sxiv")
